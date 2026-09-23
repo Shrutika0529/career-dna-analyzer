1832e3e945585eb33df54f57c02085bcae088fb4
Shrutika0529 <dse24-ssidnur@sswcoe.edu.in>
2026-09-23 18:57:32 +0530

    Add files via upload

---
+++ CareerDnaAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.careerdna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerDnaAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerDnaAnalyzerApplication.class, args);
+    }
+
+}
